22c58ca9053a641d16401d64ae8c4988d5b20a64
Andrew <morozov24.74@mail.ru>
2024-11-22 02:06:28 +0300

    final commit

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import requests
 import csv
 
@@ -5,12 +6,14 @@ def client_pars(url, number_of_users, output_file):
     response = requests.get(url, params={'number_of_users': number_of_users})
     json_data = response.json()
 
+    users = json_data['users']
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['name', 'address', 'age', 'premium_user'])
         writer.writeheader()
-        writer.writerows(json_data)
+        writer.writerows(users)
 
-    print('Данные загружены')
+    print('Данные загружены: ')
+    pprint(json_data)
 
 if __name__ == '__main__':
     client_pars('http://127.0.0.1:80/Users', 5, 'users.csv')
